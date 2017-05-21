88c245655798065383497d1c6552c8f8a3bca528
kivimango <dev@c0d3r.nhely.hu>
2017-05-21 12:37:24 +0200

    Added application icon

--- src/com/kivimango/coffeecommander/Main.java
+++ src/com/kivimango/coffeecommander/Main.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import java.awt.Dimension;
 import java.awt.Toolkit;
@@ -22,6 +23,8 @@ public class Main extends Application {
         int width = (int) (screenSize.width * 0.75);
         int height = (int) ((screenSize.height * 0.75));
         primaryStage.setScene(new Scene(root, width, height));
+
+        primaryStage.getIcons().add( new Image( Main.class.getResourceAsStream( "icon.png" )));
 
         primaryStage.show();
     }
