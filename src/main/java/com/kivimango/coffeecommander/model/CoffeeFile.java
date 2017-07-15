e037373d4226add917ff036df79d8aa4194b2836
kivimango <balazsviktor23@gmail.com>
2017-07-16 01:30:06 +0200

    Added isDir property and comparing function

--- src/main/java/com/kivimango/coffeecommander/model/CoffeeFile.java
+++ src/main/java/com/kivimango/coffeecommander/model/CoffeeFile.java
@@ -1,8 +1,9 @@
 package com.kivimango.coffeecommander.model;
 
 import javafx.scene.image.Image;
+import java.util.Comparator;
 
-public class CoffeeFile {
+public class CoffeeFile implements Comparator<CoffeeFile> {
 
     private Image icon;
     private String name;
@@ -10,15 +11,17 @@ public class CoffeeFile {
     private String date;
     private String path;
     private String permissions;
+    private boolean isDir = false;
     private static final String DIR_SZE = "<DIR>";
 
-    public CoffeeFile(Image icon, String name, long size, String date, String path, String permissions) {
+    public CoffeeFile(Image icon, String name, long size, String date, String path, String permissions, boolean isDir) {
         this.icon = icon;
         this.name = name;
         this.size = size;
         this.date = date;
         this.path = path;
         this.permissions = permissions;
+        this.isDir = isDir;
     }
 
     public Image getIcon() {
@@ -37,6 +40,7 @@ public class CoffeeFile {
     public String getPermissions() {
         return permissions;
     }
+    public boolean isDir() { return isDir; }
 
     /**
      * Converting the file's size to a human readable format.
@@ -49,5 +53,12 @@ public class CoffeeFile {
         if (size < 1024) return size + " B";
         int z = (63 - Long.numberOfLeadingZeros(size)) / 10;
         return String.format("%.1f %sB", (double)size / (1L << (z*10)), " KMGTPE".charAt(z));
+    }
+
+    @Override
+    public int compare(CoffeeFile o1, CoffeeFile o2) {
+        int b1 = o1.isDir() ? 1 : 0;
+        int b2 = o2.isDir() ? 1 : 0;
+        return b2-b1;
     }
 }
